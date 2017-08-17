81b5dcc945e3248afaa662d6e350481d43805622
Arthur517517 <arthur517517@gmail.com>
2017-08-17 09:53:37 -0700

    HelloWorld
    First program

---
+++ Project01/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * This program displays a message that says "Hello World!".
+ * 
+ * @Arthur Huang (your name) 
+ * @version (a version number or a date)
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <Arthur>"); //Display the string
+    }
+}
