fed97fad1935e2e2f206948c776d8f392029b3ef
Arthur517517 <arthur517517@gmail.com>
2017-08-23 10:50:27 -0700

    PrintPrintln
    print methods

---
+++ Project01/PrintPintln.java
@@ -0,0 +1,15 @@
+
+/**
+ * This program tests the functions of print and println.
+ *
+ * @Arthur Huang
+ * @8/23/17
+ */
+public class PrintPintln
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a text.");
+        System.out.println("Here is some more text.");
+    }
+}
