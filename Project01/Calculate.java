c7a12a9d28ce2db7868e01e0983ab472c387c724
Arthur517517 <31106977+Arthur517517@users.noreply.github.com>
2017-09-10 20:45:17 -0700

    Round

--- Project01/Calculate.java
+++ Project01/Calculate.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 /**
  * This class contains various methods that perform mathematic calculations
  * Arthur Huang
@@ -5,53 +6,66 @@
  */
 public class Calculate
 {
+    //returns a number squared
     public static int square(int number){
         return number * number;
     }
+    //returns a number cubed
     public static int cube(int number){
         return number * number * number;
     }
+    //finds the average 2 type double numbers
     public static double average(double num1, double num2){
         return (num1 + num2)/2;
     }
+    //overloaded method, finds the average of 3 numbers
     public static double average(double num1, double num2, double num3){
         return (num1 + num2 + num3)/3;
     }
+    //converts radians to degrees
     public static double toDegrees(double radian){
         double pi = 3.14159;
         return radian * (180 / pi);
     }
+    //converts degrees to radians
     public static double toRadians(double degree){
         double pi = 3.14159;
         return degree * (pi / 180);
     }
+    //uses the formula of discriminant: b^2 - 4*a*c
     public static double discriminant(double a, double b, double c){
         return (b * b) - 4 * a * c;
     }
+    //converts a mixed number to improper fraction
     public static String toImproperFrac(int wholeNum, int numerator, int denominator){
         int impNume = wholeNum * denominator + numerator;
         String improperFrac = new String(impNume + "/" + denominator);
         return improperFrac;
     }
+    //converts an improper fraction to a mixed number
     public static String toMixedNum(int numerator, int denominator){
         int wholeNum = numerator / denominator;
         int mixedNume = numerator - wholeNum * denominator;
         String mixedNum = new String(wholeNum + "_" + mixedNume + "/" + denominator);
         return mixedNum;
     }
+    //uses FOIL to return a simplified equation
     public static String foil(int a, int b, int c, int d, String x){
         String foiled = new String(a*c + x + "^2 " + "+ " + (a*d + b*c) + x +" + " + b*d);
         return foiled;
     }
+    //checks if a is divisible by b, returns a boolean value
     public static boolean isDivisibleBy(int divident, int divisor){
         return divident % divisor == 0;
     }
+    //returns the absolute value of a number
     public static double absValue(double number){
         if(number < 0){
-            number *= -1;
+            number *= -1;//converts a negative number to a positive number
         }
         return number;
     }
+    //finds the largest value between 2 numbers
     public static double max(double number1, double number2){
         double largestNum;
         if(number1 > number2){
@@ -62,6 +76,7 @@ public class Calculate
         }
         return largestNum;
     }
+    //finds the largest number between 3 numbers
     public static double max(double number1, double number2, double number3){
         double largestNum = number1;
         if(number2 > number1){
@@ -71,5 +86,20 @@ public class Calculate
             largestNum = number3;
         }
         return largestNum;
+    }
+    //finds the smallest number between 2 numebrs
+    public static int min(int number1, int number2){
+        int minNum = number1;
+        if(number2 < number1){
+            minNum = number2;
+        }
+        return minNum;
+    }
+    //rounds a double value to 2 decimal places
+    public static double round(double number){
+        String numToRound = new String(String.valueOf(number));//converts the double value into a string
+        DecimalFormat round = new DecimalFormat("#.##");
+        String numRounded = new String(round.format(number));
+        return Double.parseDouble(numRounded);//converts string value back to double
     }
 }
--- Project01/DoMath.java
+++ Project01/DoMath.java
@@ -7,12 +7,22 @@
 public class DoMath
 {
     public static void main(String[] args){
+        //The following statements calls the method in Calculate class and pint out the values
         System.out.println(Calculate.square(5));
-        System.out.println(Calculate.toMixedNum(7, 5));
+        System.out.println(Calculate.cube(79));
+        System.out.println(Calculate.average(2.4, 7.59));
+        System.out.println(Calculate.average(3.5, 7,9));
+        System.out.println(Calculate.toDegrees(6.28));
+        System.out.println(Calculate.toRadians(360));
+        System.out.println(Calculate.discriminant(3, 4, 5));
         System.out.println(Calculate.toImproperFrac(3, 1, 2));
+        System.out.println(Calculate.toMixedNum(7, 5));
         System.out.println(Calculate.foil(3, 4, 5 ,6, "x"));
         System.out.println(Calculate.isDivisibleBy(40, 8));
         System.out.println(Calculate.absValue(-0.876));
-        System.out.println(Calculate.max(3,7));
+        System.out.println(Calculate.max(3, 7));
+        System.out.println(Calculate.max(4.5, 7.8 , -10.9));
+        System.out.println(Calculate.min(1, -1));
+        System.out.println(Calculate.round(4.7850987));
     }
 }
