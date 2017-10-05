1b25c42eb60ad9acf626931520f57a817c26c14f
Arthur517517 <31106977+Arthur517517@users.noreply.github.com>
2017-10-04 22:32:10 -0700

    ProcessingNumbers Comments

--- Project01/ProcessingNumbers.java
+++ Project01/ProcessingNumbers.java
@@ -9,43 +9,44 @@ import java.util.Scanner;
 public class ProcessingNumbers
 {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int numInput;
+        Scanner input = new Scanner(System.in);//initializes a Scanner object
+        int numInput;//declare variables 
         int userInput;
         int smallestNum;
         int largestNum;
         int largestEven;
         int evenSum = 0;
+        
         System.out.println("How many numbers are you entering?");
         numInput = input.nextInt();
-        
         System.out.println("Enter a number: ");
         userInput = input.nextInt();
         input.nextLine();
-        largestNum = userInput;
+        largestNum = userInput;//gets the first number and set it to the largest number
         smallestNum = userInput;
         largestEven = userInput;
+        
         if(userInput % 2 == 0){
             evenSum += userInput;
         }
         for(int i = 1; i < numInput; i++){
             System.out.println("Enter a number: ");
             userInput = input.nextInt();
-            if(userInput > largestNum){
+            if(userInput > largestNum){//if the current number is greater than the largest number
                 largestNum = userInput;
             }
             if(userInput < smallestNum){
                 smallestNum = userInput;
             }
-            if(userInput > largestEven && userInput % 2 == 0){
+            if(userInput > largestEven && userInput % 2 == 0){//if the current number is greater than the largest even number, and is divisible by 2
                 largestEven = userInput;
             }
             if(userInput % 2 == 0){
                 evenSum += userInput;
             }
         }
-        System.out.println("The largest number is " + largestNum + ", the smallest number is " + smallestNum);
-        System.out.println("The sum of all numbers is " + evenSum);
+        System.out.println("The largest number is " + largestNum + ", the smallest number is " + smallestNum);//print the largest number and the smallest number
+        System.out.println("The sum of all even numbers is " + evenSum);
         System.out.println("The largest even number is " + largestEven);
 
         
