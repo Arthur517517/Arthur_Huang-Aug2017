475e4947d39e8394bf1fbd8b98f3f708ebd75489
Arthur517517 <31106977+Arthur517517@users.noreply.github.com>
2017-10-16 21:47:58 -0700

    Quadratic Edit

--- Project01/QuadraticClient.java
+++ Project01/QuadraticClient.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class QuadraticClient here.
  *
@@ -8,6 +8,28 @@
 public class QuadraticClient
 {
     public static void main(String[] args){
-        System.out.println(Quadratic.quadrDescriber(-1, 4, -5));
+        boolean done = false;
+        Scanner input = new Scanner(System.in);
+        double a;
+        double b;
+        double c;
+        String finished;
+        while(!done){
+            System.out.println("Welcome to the Quadratic Describer");
+            System.out.println("Please provide values for coefficients a, b, and c\n");
+            System.out.print("a: ");
+            a = input.nextDouble();
+            System.out.print("b: ");
+            b = input.nextDouble();
+            System.out.print("c: ");
+            c = input.nextDouble();
+            System.out.println(Quadratic.quadrDescriber(a, b, c));
+            
+            System.out.println("Do you want ot keep going? (Type \"quit\" to end)");
+            finished = input.next();
+            if(finished.equals("quit")){
+                done = true;
+            }
+        }
     }
 }
