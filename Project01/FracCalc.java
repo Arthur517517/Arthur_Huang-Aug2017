dbad19772e06b9f6dcc2cce1023f0c15279e9d54
Arthur517517 <arthur517517@gmail.com>
2017-11-13 09:52:51 -0800

    frac checkpoint 1

--- Project01/FracCalc.java
+++ Project01/FracCalc.java
@@ -1,11 +1,14 @@
- 
-
+import java.util.Scanner;
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+        String userExpr;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your expression: ");
+        userExpr = input.next();
+        produceAnswer(userExpr);
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -19,8 +22,11 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+        String splitArr[] = input.split(" ");
+        String firstOperand = splitArr[0];
+        String operator = splitArr[1];
+        String secondOperand = splitArr[splitArr.length - 1];
+        return secondOperand;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
