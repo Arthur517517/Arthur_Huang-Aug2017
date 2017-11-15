635f29fd5da18c41f558989efa4d896e625ac925
Arthur517517 <arthur517517@gmail.com>
2017-11-15 10:57:36 -0800

    FracCalc checkpoint 2

--- Project01/FracCalc.java
+++ Project01/FracCalc.java
@@ -4,11 +4,17 @@ public class FracCalc {
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
+        boolean done = false;
         String userExpr;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your expression: ");
-        userExpr = input.next();
-        produceAnswer(userExpr);
+        while(!done){
+            System.out.println("Enter your expression: ");
+            userExpr = input.next();
+            System.out.println(produceAnswer(userExpr));
+            if(userExpr.equals("quit")){
+                done = true;
+            }
+        }
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -22,13 +28,41 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-        String splitArr[] = input.split(" ");
+        String[] splitArr = input.split(" ");
         String firstOperand = splitArr[0];
         String operator = splitArr[1];
         String secondOperand = splitArr[splitArr.length - 1];
-        return secondOperand;
+        
+        return parseOperands(secondOperand);
     }
+    
 
     // TODO: Fill in the space below with any helper methods that you think you will need
-    
+    public static String parseOperands(String operand){
+        String wholeNumber = "";
+        String numerator = "";
+        String denominator = "";
+        String[] slashUnderscore;
+        String[] splitUnderscore = operand.split("_");
+        if(splitUnderscore.length == 2){
+            wholeNumber = splitUnderscore[0];
+            slashUnderscore = splitUnderscore[1].split("/");
+            if(slashUnderscore.length == 2){
+                numerator = slashUnderscore[0];
+                denominator = slashUnderscore[1];
+            }
+        }else{
+            slashUnderscore = operand.split("/");
+            if(slashUnderscore.length == 2){
+                wholeNumber = "0";
+                numerator = slashUnderscore[0];
+                denominator = slashUnderscore[1];
+            }else{
+                wholeNumber = splitUnderscore[0];
+                numerator = "0";
+                denominator = "1";
+            }
+        }
+        return "whole:" + wholeNumber + " numerator:" + numerator + " denominator:" + denominator;
+    }
 }
