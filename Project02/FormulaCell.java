b041e6d4f616d3513247b71390723d22c86ed5f1
Arthur517517 <arthur517517@gmail.com>
2018-03-28 10:32:59 -0700

    cp4

--- Project02/FormulaCell.java
+++ Project02/FormulaCell.java
@@ -6,7 +6,12 @@
 	
 	public String abbreviatedCellText() {
 		String finalString = getDoubleValue() + "";
-		return fillSpaces(finalString);
+		if(finalString.length() < 10) {
+			return fillSpaces(finalString);
+		}else {
+			return finalString.substring(0, 10);
+		}
+		
 	}
 
 	public String fullCellText() {
@@ -16,7 +21,37 @@
 	public double getDoubleValue() {
 		String modified = getRealCell().substring(2, getRealCell().length()-2);
 		String[] arr = modified.split(" ");
+		double result = 0;
+		if(arr.length == 1) {
+			result = Double.parseDouble(arr[0]);
+		}else {
+			if(arr[1].equals("+")) {
+				result = Double.parseDouble(arr[0]) + Double.parseDouble(arr[2]);
+			}else if(arr[1].equals("-")) {
+				result = Double.parseDouble(arr[0]) - Double.parseDouble(arr[2]);
+			}else if(arr[1].equals("*")) {
+				result = Double.parseDouble(arr[0]) * Double.parseDouble(arr[2]);
+			}else if(arr[1].equals("/")){
+				result = Double.parseDouble(arr[0]) / Double.parseDouble(arr[2]);
+			}
+		}
 		
+		if(arr.length == 1) {
+			result = Double.parseDouble(arr[0]);
+		}else{
+			for(int i = 2; i < arr.length; i++) {
+				if(arr[i].equals("+")){
+					result += Double.parseDouble(arr[i+1]);
+				}else if(arr[i].equals("-")) {
+					result -= Double.parseDouble(arr[i+1]);
+				}else if(arr[i].equals("*")){
+					result *= Double.parseDouble(arr[i+1]);
+				}else if(arr[i].equals("/")){
+					result /= Double.parseDouble(arr[i+1]);
+				}
+			}
+		}
+		return result;
 	}
 }
 
--- Project02/ValueCell.java
+++ Project02/ValueCell.java
@@ -1,21 +1,18 @@
-public class ValueCell extends RealCell{
+	public class ValueCell extends RealCell{
 	public ValueCell(String entry) {
 		super(entry);
 	}
 	public String abbreviatedCellText() {
-		String cellText = Double.toString(getDoubleValue());
-		if(cellText.length() < 10) {
-			if(!cellText.contains(".")) {    //checks if its a whole number 
-				cellText += ".0";
-			}
-			return super.fillSpaces(cellText);
+		String txt = getDoubleValue() + "";
+		if(txt.length() < 10) {
+			return super.fillSpaces(txt);
 		}else {	
-			return cellText.substring(0,10);
+			return txt.substring(0,10);
 		}
 	}
 	
 	public String fullCellText() {
-		return getRealCell();
+		return super.getRealCell();
 	}
 	
 	public double getDoubleValue() {
